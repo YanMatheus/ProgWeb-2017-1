86e458ceec7d704a187df9810e513feaa254a870
YanMatheus <ymgf@icomp.ufam.edu.br>
2017-05-03 10:14:02 -0400

    Add files via upload

---
+++ questao_03.js
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Instituto de Computação</title>
+
+
+
+  </head>
+  <body>
+
+    <form >
+
+      <div style = " background : yellow; width: 100px; height: 100px;" >
+
+        Canto superior esquerdo
+
+      </div>
+    </form>
+
+    <form >
+
+      <div >
+<style media="screen">
+  background : blue; width: 100px; height: 100px;
+</style>
+        Canto inferior esquedo
+
+      </div>
+    </form>
+
+    <form >
+
+      <div style = " background : red; width: 100px; height: 100px;" >
+
+        Canto superior direito
+
+      </div>
+    </form>
+
+
+    <form >
+
+      <div style = " background : green; width: 100px; height: 100px;" >
+
+        Canto inferior direito
+
+      </div>
+    </form>
+
+  </script>
+
+  </body>
+</html>
