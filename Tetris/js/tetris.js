cceb736fa5c500769931e117544f36b8eb3ff75b
Unknown <ymgf@icomp.ufam.edu.br>
2017-06-07 01:19:50 -0400

    descendo, controle feito, css feito

--- Tetris/js/tetris.js
+++ Tetris/js/tetris.js
@@ -1,36 +1,39 @@
-(function () { // #TO-DO: Colocar uma action no botao e iniciar o jogo a partir dele
+(function() {
 
-    var proximaPeca, tabuleiro, peca, proximaPecaTable, informacoes, fps = 10, gameLoop; 
-    var linha = 0, coluna = 4, celula;
+    var proximaPeca, tabuleiro, peca, proximaPecaTable, informacoes, fps = 10,
+        gameLoop;
+    var linha = 0,
+        coluna = 4,
+        celula;
 
     addEventListener("keydown", function(e) {
-    	var prox_celula;
-        if (e.key == "ArrowRight") { //Move a peça para a direita, se permitido.
-            if(coluna != 9){ //Verifica se atingiu o limite horizontal esquerdo.
-                prox_celula = document.getElementById("celula "+linha+" "+(coluna+1)); 
-            	if(prox_celula.style.backgroundColor == ""){ //Verifica se existe uma peça à direita da atual. Se não existir, move a peça.
-            		celula.style.backgroundColor = "";
-                	coluna++;
-                	celula = document.getElementById("celula "+linha+" "+coluna);
-        			celula.style.backgroundColor = "green";
-            	}
+        var prox_celula;
+        if (e.key == "ArrowRight") {
+            if (coluna != 9) {
+                prox_celula = document.getElementById("celula " + linha + " " + (coluna + 1));
+                if (prox_celula.style.backgroundColor == "") { //Verifica se existe uma peça à direita da atual. Se não existir, move a peça.
+                    celula.style.backgroundColor = "";
+                    coluna++;
+                    celula = document.getElementById("celula " + linha + " " + coluna);
+                    celula.style.backgroundColor = "green";
+                }
             }
         } else if (e.key == "ArrowLeft") { //Move a peça para a esquerda, se permitido.
-            if(coluna != 0){ //Verifica se atingiu o limite horizontal direito.
-                prox_celula = document.getElementById("celula "+linha+" "+(coluna-1)); 
-            	if(prox_celula.style.backgroundColor == ""){ //Verifica se existe uma peça à esquerda da atual. Se não existir, move a peça.
-            		celula.style.backgroundColor = "";
-                	coluna--;
-                	celula = document.getElementById("celula "+linha+" "+coluna);
-        			celula.style.backgroundColor = "green";
-            	}
+            if (coluna != 0) { //Verifica se atingiu o limite horizontal direito.
+                prox_celula = document.getElementById("celula " + linha + " " + (coluna - 1));
+                if (prox_celula.style.backgroundColor == "") { //Verifica se existe uma peça à esquerda da atual. Se não existir, move a peça.
+                    celula.style.backgroundColor = "";
+                    coluna--;
+                    celula = document.getElementById("celula " + linha + " " + coluna);
+                    celula.style.backgroundColor = "green";
+                }
             }
         } else if (e.key == "ArrowDown") {
             run();
         }
     });
 
-    function init () {
+    function init() {
         tabuleiro = document.querySelector("#tabuleiro");
         informacoes = document.querySelector("#informacoes");
         proximaPeca = document.querySelector("#proxima-peca");
@@ -42,85 +45,85 @@
         botao.appendChild(botaoTextNode);
         proximaPeca.appendChild(botao);
         console.log(botao);
-        gameLoop = setInterval(run, 5500/fps);
+        gameLoop = setInterval(run, 5500 / fps);
     }
 
-    function run () {
+    function run() {
         var prox_celula;
 
-        if(linha != 19){ //Descolore a celula anterior e colore a próxima celula caso a peça não esteja no final da tabela ou não encontre 
-        				 //outra peça em sua próxima posição.
-            prox_celula = document.getElementById("celula "+(linha+1)+" "+coluna);
-            if(prox_celula.style.backgroundColor == "")
+        if (linha != 19) { //Descolore a celula anterior e colore a próxima celula caso a peça não esteja no final da tabela ou não encontre
+            //outra peça em sua próxima posição.
+            prox_celula = document.getElementById("celula " + (linha + 1) + " " + coluna);
+            if (prox_celula.style.backgroundColor == "")
                 celula.style.backgroundColor = "";
         }
 
         linha++;
-        prox_celula = document.getElementById("celula "+linha+" "+coluna);
-        if(linha == 20 || prox_celula.style.backgroundColor != ""){ //Reseta a posição da peça caso a peça chegue no final da tabela ou
-            linha = 0;                                              //caso a próxima posição da peça esteja ocupada.
-            coluna = 4; 
+        prox_celula = document.getElementById("celula " + linha + " " + coluna);
+        if (linha == 20 || prox_celula.style.backgroundColor != "") { //Reseta a posição da peça caso a peça chegue no final da tabela ou
+            linha = 0; //caso a próxima posição da peça esteja ocupada.
+            coluna = 4;
         }
-        celula = document.getElementById("celula "+linha+" "+coluna);
+        celula = document.getElementById("celula " + linha + " " + coluna);
         celula.style.backgroundColor = "green";
 
-      //  if (posicao >= 650) clearInterval(gameLoop);       // **ISTO AQUI SIGNFICA QUANDO A TABELA FICA CHEIA DE PEÇAS E O JOGO DÁ GAMEOVER
-        
+          if (posicao >= 650) clearInterval(gameLoop);       // **ISTO AQUI SIGNFICA QUANDO A TABELA FICA CHEIA DE PEÇAS E O JOGO DÁ GAMEOVER
+
     }
 
-    function createPeca () {
+    function createPeca() {
         celula = document.getElementById("celula 0 4");
         //peca = document.createElement("div");
-        celula.style.backgroundColor="green";
+        celula.style.backgroundColor = "green";
         //tabuleiro.appendChild(peca);
     }
 
-    function createTabuleiro(){
+    function createTabuleiro() {
         var i, j, tr, td, tabela = document.createElement("table");
 
-        for(i = 0; i<20; i++){
+        for (i = 0; i < 20; i++) {
             tr = document.createElement("tr");
 
-            for(j = 0; j<10; j++){
+            for (j = 0; j < 10; j++) {
                 td = document.createElement("td");
-                td.id = "celula "+i+" "+j;
+                td.id = "celula " + i + " " + j;
                 tr.appendChild(td);
             }
             tabela.appendChild(tr);
         }
-        
+
         tabuleiro.appendChild(tabela)
     }
 
-    function createProximaPecaTable(){
+    function createProximaPecaTable() {
         var i, j, tr, td;
         proximaPecaTable = document.createElement("table")
         proximaPecaTable.id = "proximaPecaTable";
-        for(i = 0; i<4; i++){
+        for (i = 0; i < 4; i++) {
             tr = document.createElement("tr");
 
-            for(j = 0; j<4; j++){
+            for (j = 0; j < 4; j++) {
                 td = document.createElement("td");
-                td.id = "proximaPecaTable "+i+" "+j;
+                td.id = "proximaPecaTable " + i + " " + j;
                 tr.appendChild(td);
             }
             proximaPecaTable.appendChild(tr);
         }
 
         proximaPeca.appendChild(proximaPecaTable)
-        
+
     }
 
-    function geraPeca(){
+    function geraPeca() {
         var i, j, random = Math.floor(Math.random() * 7);
 
-        for(i = 0; i<4; i++)
-            for(j = 0; j<4; j++){
-                td = document.getElementById("proximaPecaTable "+i+" "+j);
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 4; j++) {
+                td = document.getElementById("proximaPecaTable " + i + " " + j);
                 td.style.backgroundColor = "";
             }
 
-        switch(random){
+        switch (random) {
             case 0:
                 td = document.getElementById("proximaPecaTable 1 0");
                 td.style.backgroundColor = "cyan";
@@ -193,6 +196,6 @@
                 break;
         }
     }
-// style="background-color: red; border: 1px solid #000 margin: 0px"
+  //  style="background-color: red; border: 1px solid #000 margin: 0px"
     init();
 })();
--- Tetris/js/tetrisv2.js
+++ Tetris/js/tetrisv2.js
@@ -1,5 +1,4 @@
-
-(function () {
+(function() {
 
     var tabuleiro;
     var peca;
@@ -8,21 +7,21 @@
 
     addEventListener("keydown", function(e) {
         if (e.key == "ArrowRight") {
-            if(!(parseInt(peca.style.left) >= 308))
+            if (!(parseInt(peca.style.left) >= 308))
                 peca.style.left = (parseInt(peca.style.left) + 34) + "px";
         } else if (e.key == "ArrowLeft") {
-            if(!(parseInt(peca.style.left) <= 18))
+            if (!(parseInt(peca.style.left) <= 18))
                 peca.style.left = (parseInt(peca.style.left) - 34) + "px";
         }
     });
 
-    function init () {
+    function init() {
         createTabuleiro();
         createPeca();
-        gameLoop = setInterval(run, 1500/fps);
+        gameLoop = setInterval(run, 1500 / fps);
     }
 
-    function run () {
+    function run() {
         var posicao = parseInt(peca.style.top);
         posicao = posicao + 17;
         if (posicao >= 650) {
@@ -31,7 +30,7 @@
         peca.style.top = posicao + "px";
     }
 
-    function createPeca () {
+    function createPeca() {
         tabuleiro = document.querySelector("#tabuleiro");
         peca = document.createElement("div");
         peca.style.top = "12px";
@@ -39,19 +38,19 @@
         tabuleiro.appendChild(peca);
     }
 
-    function createTabuleiro(){
+    function createTabuleiro() {
         tabuleiro = document.querySelector("#tabuleiro");
         var tabela = document.createElement("table");
         tabela.cellPadding = "15px";
         var i, j;
         var tr, td, texto;
 
-        for(i = 0; i<20; i++){
+        for (i = 0; i < 20; i++) {
             tr = document.createElement("tr");
 
-            for(j = 0; j<10; j++){
+            for (j = 0; j < 10; j++) {
                 td = document.createElement("td");
-                td.id = "celula "+i+" "+j;
+                td.id = "celula " + i + " " + j;
                 texto = document.createTextNode(" ");
                 td.appendChild(texto);
                 tr.appendChild(td);
@@ -60,7 +59,7 @@
         }
 
         tabuleiro.appendChild(tabela)
-        tabela.setAttribute("border","1");
+        tabela.setAttribute("border", "1");
 
     }
 
